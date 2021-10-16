1ef6d23fc18c623b7f1ebeaeabca036193df4998
Jonathan Barrios <jonathanbarrios81@gmail.com>
2021-10-16 17:23:10 -0400

    add recent

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -1,11 +1,28 @@
+from math import sqrt
 from typing import List
 
 from test_framework import generic_test
 
+import sortedcontainers as sc
 
-def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
+
+def generate_first_k_a_b_sqrt2(k) -> List[float]:
     # TODO - you fill in here.
-    return []
+    btree = sc.SortedSet()
+    # f = a + b * 1.414
+    def f(a, b): return a+b*sqrt(2)
+
+    btree.add((0, (0, 0)))
+    result = []
+    for idx in range(k):
+        value, startPair = btree.pop(0)
+
+        currA, currB = startPair
+        btree.add((f(currA+1, currB), (currA+1, currB)))
+        btree.add((f(currA, currB+1), (currA, currB+1)))
+        result.append(value)
+
+    return result
 
 
 if __name__ == '__main__':
